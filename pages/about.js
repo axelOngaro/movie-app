e20622779949e6268727e8a046a9e9b89db938b1
Axel <axel.ongaro.pro@gmail.com>
2021-01-13 09:29:05 +0100

    Added a class component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,14 @@
 import React from "react";
-const About = () => {
-  const message = "Hello World";
-  return <h1>About page - {message}</h1>;
-};
+// const About = () => {
+//   const message = "Hello World";
+//   return <h1>About page - {message}</h1>;
+// };
+
+// class component
+class About extends React.Component {
+  render() {
+    return <h1>Hello this is a class component</h1>;
+  }
+}
 
 export default About;
